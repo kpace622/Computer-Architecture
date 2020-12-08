7d2ddac5891b8b9f77efed62596a6a5a88252a8d
Kyle Pace <kpace2296@gmail.com>
2020-12-07 20:03:34 -0600

    finished day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.registers = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -27,9 +29,9 @@ class CPU:
         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            # self.ram[address] = int(instruction[0:8], 2)
+            self.ram_write(address, instruction)
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -59,7 +61,42 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+    
+    def ram_read(self, idx):
+        return self.ram[idx]
+
+    def ram_write(self, idx, value):
+        self.ram[idx] = value
 
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+
+        running = True
+        
+        while running:
+
+            command = self.ram_read(self.pc)
+            
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if command == HLT:
+                running = False
+
+            elif command == LDI:
+                self.registers[operand_a] = operand_b
+                self.pc += 2
+
+            elif command == PRN:
+                print(self.registers[operand_a])
+                self.pc += 2
+            
+            else: 
+                self.pc+=1
+
+            # print('operand_a', operand_a)
+            # command = self.ram[self.pc]
+            # self.pc += 1
